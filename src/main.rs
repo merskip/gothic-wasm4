be26ddbff60b48159dcb0199ec65d00e7d909790
Piotr Merski <merskip@gmail.com>
2023-03-02 19:57:05 +0100

    Added hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use wasm4::application::Application;
+use wasm4::framebuffer::Framebuffer;
+use wasm4::geometry::Point;
+use wasm4::main_application;
+
+struct GothicApplication {
+    framebuffer: Framebuffer
+}
+
+impl Application for GothicApplication {
+    fn start(framebuffer: Framebuffer) -> Self {
+        Self {
+            framebuffer
+        }
+    }
+
+    fn update(&mut self) {
+        self.framebuffer.text("Gothic", Point::new(0, 0));
+    }
+}
+
+main_application! { GothicApplication }
+
+#[panic_handler]
+#[cfg(not(test))]
+fn panic_handler(_panic_info: &PanicInfo<'_>) -> ! {
+    core::arch::wasm32::unreachable();
 }
