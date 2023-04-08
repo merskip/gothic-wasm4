f2634b0f06e30bc6a23831a327682c5a19b17286
Piotr Merski <merskip@gmail.com>
2023-04-08 16:11:13 +0200

    Scrolling text in cinematic

--- gothic/src/ui/cinematic/cinematic_player.rs
+++ gothic/src/ui/cinematic/cinematic_player.rs
@@ -8,12 +8,13 @@ use wasm4::inputs::Inputs;
 use crate::dispatcher::Dispatcher;
 use crate::renderable::Renderable;
 use crate::ui::cinematic::cinematic::Cinematic;
-use crate::ui::cinematic::cinematic_screen::CinematicScreen;
+use crate::ui::cinematic::cinematic_screen::{CinematicScreen, CinematicScreenView};
 use crate::updatable::Updatable;
 
 pub struct CinematicPlayer {
     cinematic: &'static Cinematic,
     current_screen_index: usize,
+    current_screen: CinematicScreenView,
     finish_handler: Rc<dyn Fn()>,
 }
 
@@ -21,18 +22,16 @@ impl CinematicPlayer {
     pub fn new(cinematic: &'static Cinematic, finish_handler: Rc<dyn Fn()>) -> Self {
         Self {
             cinematic,
+            current_screen: CinematicScreenView::new(&cinematic.screens[0]),
             current_screen_index: 0,
-            finish_handler
+            finish_handler,
         }
-    }
-
-    fn get_current_screen(&self) -> &'static CinematicScreen {
-        &self.cinematic.screens[self.current_screen_index]
     }
 
     fn show_next_screen(&mut self) {
         self.current_screen_index = (self.current_screen_index + 1)
             .clamp(0, self.cinematic.screens.len() - 1);
+        self.current_screen = CinematicScreenView::new(&self.cinematic.screens[self.current_screen_index]);
     }
 
     fn is_last_screen(&self) -> bool {
@@ -42,7 +41,10 @@ impl CinematicPlayer {
 
 impl Updatable for CinematicPlayer {
     fn update(&mut self, inputs: &Inputs, dispatcher: &mut Dispatcher) {
-        if inputs.gamepad1.is_released(ButtonX) {
+        self.current_screen.update(inputs, dispatcher);
+
+        if self.current_screen.is_finished()
+            && inputs.gamepad1.is_released(ButtonX) {
             if self.is_last_screen() {
                 let finish_handler = self.finish_handler.clone();
                 dispatcher.dispatch(Box::new(move ||
@@ -57,9 +59,6 @@ impl Updatable for CinematicPlayer {
 
 impl Renderable for CinematicPlayer {
     fn render(&self, framebuffer: &Framebuffer, frame: Rect) {
-        self.get_current_screen().render(framebuffer, frame);
-
-        framebuffer.set_draw_color(DrawColorIndex::Index1, Palette4);
-        framebuffer.text("Press X to continue", Point::new(5, 150));
+        self.current_screen.render(framebuffer, frame);
     }
 }
