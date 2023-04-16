9d0af3cf927d03ca15ebea7bf95d7aa30f230a71
Piotr Merski <merskip@gmail.com>
2023-04-16 13:37:31 +0200

    Use WASM-4 only for wasm-4 target

--- gothic/src/bin/wasm-4.rs
+++ gothic/src/bin/wasm-4.rs
@@ -3,12 +3,11 @@
 
 extern crate alloc;
 
-#[cfg(not(test))]
 use core::panic::PanicInfo;
 use gothic::audio::music::Music;
 use gothic::dispatcher::Dispatcher;
 use gothic::game::main_menu::make_main_menu;
-use gothic::music_clips;
+use gothic::{GothicApplication, music_clips};
 use gothic::renderable::RenderContext;
 use gothic::ui::navigator::Navigator;
 use gothic::updatable::UpdateContext;
@@ -19,55 +18,6 @@ use wasm4::framebuffer::Framebuffer;
 use wasm4::geometry::{Point, Rect};
 use wasm4::inputs::Inputs;
 use wasm4::{main_application};
-
-struct GothicApplication {
-    dispatcher: Dispatcher,
-    navigator: Navigator,
-    music: Music,
-}
-
-impl Application for GothicApplication {
-    fn start() -> Self {
-        let mut navigator = Navigator::new();
-        navigator.push_view(make_main_menu());
-
-        let mut music = Music::new(Audio::shared());
-        music.play_clip(&music_clips::MAIN_THEME);
-
-        Self {
-            dispatcher: Dispatcher::new(),
-            navigator,
-            music,
-        }
-    }
-
-    fn update(&mut self, inputs: &Inputs) {
-        let mut context = UpdateContext::new(
-            &mut self.dispatcher,
-            &mut self.navigator,
-            inputs,
-            &mut self.music,
-        );
-
-        if let Some(mut top_view) = context.navigator.top_view_mut() {
-            top_view.update(&mut context);
-            context.navigator.push_view_box(top_view);
-        }
-        context.music.update();
-        context.dispatcher.execute(&mut context);
-    }
-
-    fn render(&self, framebuffer: &Framebuffer) {
-        let frame = Rect::new(
-            Point::new(0, 0),
-            framebuffer.get_size(),
-        );
-        if let Some(top_view) = self.navigator.top_view() {
-            let mut context = RenderContext::new(framebuffer, frame);
-            top_view.render(&mut context);
-        }
-    }
-}
 
 main_application! { GothicApplication }
 
--- gothic/src/bin/windows.rs
+++ gothic/src/bin/windows.rs
@@ -1,4 +1,78 @@
+use std::os::raw::c_void;
+
+use gothic::GothicApplication;
+use wasm4::application::Application;
+use wasm4::gamepad::Gamepad;
+use wasm4::inputs::Inputs;
+
+#[no_mangle]
+pub fn rect(x: i32, y: i32, width: u32, height: u32) {}
+
+#[no_mangle]
+pub fn oval(x: i32, y: i32, width: u32, height: u32) {}
+
+#[no_mangle]
+pub fn line(x1: i32, y1: i32, x2: i32, y2: i32) {}
+
+#[no_mangle]
+pub fn hline(x: i32, y: i32, len: u32) {}
+
+#[no_mangle]
+pub fn vline(x: i32, y: i32, len: u32) {}
+
+#[no_mangle]
+pub fn text(text: *const u8, x: i32, y: i32) {}
+
+#[no_mangle]
+pub fn textUtf8(text: *const u8, byte_length: usize, x: i32, y: i32) {}
+
+#[no_mangle]
+pub fn textUtf16(text: *const u16, byte_length: usize, x: i32, y: i32) {}
+
+#[no_mangle]
+pub fn blit(sprite: *const u8, x: i32, y: i32, width: u32, height: u32, flags: u32) {}
+
+#[no_mangle]
+pub fn blitSub(
+    sprite: *const u8,
+    x: i32,
+    y: i32,
+    width: u32,
+    height: u32,
+    src_x: u32,
+    src_y: u32,
+    stride: u32,
+    flags: u32,
+) {}
+
+#[no_mangle]
+pub fn tone(frequency: u32, duration: u32, volume: u32, flags: u32) {
+    println!("frequency: {}, duration: {}, volume: {}, flags: {}", frequency, duration, volume, flags);
+}
+
+#[no_mangle]
+pub fn diskr(dest: *mut u8, size: usize) -> u32 { 0 }
+
+#[no_mangle]
+pub fn diskw(src: *const u8, size: usize) -> u32 { 0 }
+
+#[no_mangle]
+pub fn trace(trace: *const u8) {}
+
+#[no_mangle]
+pub fn traceUtf8(trace: *const u8, byte_length: usize) {}
+
+#[no_mangle]
+pub fn traceUtf16(trace: *const u16, byte_length: usize) {}
+
+#[no_mangle]
+pub fn tracef(fmt: *const u8, stack_ptr: *const c_void) {}
 
 fn main() {
+    let mut application = GothicApplication::start();
+
+    let inputs = unsafe { Inputs::new() };
+    application.update(&inputs);
+
     println!("Hello world!");
 }
