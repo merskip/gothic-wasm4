07a50c147e3624e96cd036e887924c9a8d2b587e
Piotr Merski <merskip@gmail.com>
2023-04-23 06:40:52 +0200

    Simple drawing using Direct2D

--- gothic/src/bin/windows/application_window.rs
+++ gothic/src/bin/windows/application_window.rs
@@ -1,43 +1,44 @@
-use std::iter::Peekable;
-
+use windows::core::{ComInterface, Interface, Result};
+use windows::Foundation::Numerics::Matrix3x2;
 use windows::s;
 use windows::Win32::Foundation::*;
 use windows::Win32::Graphics::Direct2D::*;
-use windows::Win32::Graphics::Dxgi::CreateDXGIFactory1;
-use windows::Win32::Graphics::Gdi::{BeginPaint, EndPaint, PAINTSTRUCT};
+use windows::Win32::Graphics::Direct2D::Common::{D2D1_ALPHA_MODE_IGNORE, D2D1_COLOR_F, D2D1_PIXEL_FORMAT, D2D_RECT_F, D2D_SIZE_U};
+use windows::Win32::Graphics::Direct3D::*;
+use windows::Win32::Graphics::Direct3D11::*;
+use windows::Win32::Graphics::Dxgi::*;
+use windows::Win32::Graphics::Dxgi::Common::{DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_SAMPLE_DESC};
+use windows::Win32::Graphics::Gdi::*;
 use windows::Win32::System::LibraryLoader::GetModuleHandleA;
 use windows::Win32::UI::WindowsAndMessaging::*;
 
 pub struct ApplicationWindow {
     window_handle: HWND,
-    // factory: ID2D1Factory1,
-    // dx_factory: ID2D1Factory2,
+    factory: ID2D1Factory1,
+    dx_factory: IDXGIFactory2,
     visible: bool,
+    target: Option<ID2D1DeviceContext>,
+    swapchain: Option<IDXGISwapChain1>,
+    brush: Option<ID2D1SolidColorBrush>,
 }
 
 impl ApplicationWindow {
-    pub fn new() -> windows::core::Result<Self> {
-        // let factory = unsafe {
-        //     let mut options = D2D1_FACTORY_OPTIONS::default();
-        //     if cfg!(debug_assertions) {
-        //         options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
-        //     }
-        //     D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, Some(&options))?
-        // };
-        //
-        // let dx_factory = unsafe {
-        //     CreateDXGIFactory1()?
-        // };
+    pub fn new() -> Result<Self> {
+        let factory = create_factory()?;
+        let dx_factory = unsafe { CreateDXGIFactory1()? };
 
         Ok(Self {
             window_handle: HWND(0),
-            // factory,
-            // dx_factory,
+            factory,
+            dx_factory,
             visible: false,
+            target: None,
+            swapchain: None,
+            brush: None,
         })
     }
 
-    pub fn run(&mut self) -> windows::core::Result<()> {
+    pub fn run(&mut self) -> Result<()> {
         unsafe {
             let instance = GetModuleHandleA(None)?;
             debug_assert!(!instance.is_invalid());
@@ -73,18 +74,26 @@ impl ApplicationWindow {
             debug_assert!(window_handle.0 != 0);
             debug_assert!(self.window_handle == window_handle);
 
-            let mut message = MSG::default();
-            loop {
-                if self.visible {
-                    self.render()?;
+            self.run_message_loop()
+        }
+    }
 
+    fn run_message_loop(&mut self) -> Result<()> {
+        let mut message = MSG::default();
+        loop {
+            if self.visible {
+                self.render()?;
+
+                unsafe {
                     while PeekMessageA(&mut message, None, 0, 0, PM_REMOVE).into() {
                         if message.message == WM_QUIT {
                             return Ok(());
                         }
                         DispatchMessageA(&message);
                     }
-                } else {
+                }
+            } else {
+                unsafe {
                     GetMessageA(&mut message, None, 0, 0);
                     if message.message == WM_QUIT {
                         return Ok(());
@@ -107,6 +116,7 @@ impl ApplicationWindow {
                     let create = lparam.0 as *const CREATESTRUCTA;
                     let this = (*create).lpCreateParams as *mut Self;
                     (*this).window_handle = window;
+                    SetWindowLongPtrA(window, GWLP_USERDATA, this as _);
                 }
                 _ => {
                     let this = GetWindowLongPtrA(window, GWLP_USERDATA) as *mut Self;
@@ -146,7 +156,202 @@ impl ApplicationWindow {
         }
     }
 
-    fn render(&mut self) -> windows::core::Result<()> {
+    fn render(&mut self) -> Result<()> {
+        if self.target.is_none() {
+            let device = create_device()?;
+            let target = create_render_target(&self.factory, &device)?;
+            let swapchain = create_swapchain(&device, self.window_handle)?;
+            create_swapchain_bitmap(&swapchain, &target)?;
+
+            self.brush = create_brush(&target).ok();
+            self.target = Some(target);
+            self.swapchain = Some(swapchain);
+        }
+
+
+        let target = self.target.as_ref().unwrap();
+        unsafe {
+            target.BeginDraw();
+        };
+        self.draw(&target)?;
+
+        unsafe {
+            target.EndDraw(None, None)?;
+        }
+
+        if let Err(error) = self.present(1, 0) {
+            if error.code() == DXGI_STATUS_OCCLUDED {
+                // self.occlusion = unsafe {
+                //     self.dxfactory
+                //         .RegisterOcclusionStatusWindow(self.handle, WM_USER)?
+                // };
+                self.visible = false;
+            } else {
+                // self.release_device();
+            }
+        }
+
         Ok(())
     }
+
+    fn draw(&self, target: &ID2D1DeviceContext) -> Result<()> {
+        unsafe {
+            target.Clear(Some(&D2D1_COLOR_F {
+                r: 1.0,
+                g: 1.0,
+                b: 1.0,
+                a: 1.0,
+            }));
+
+            let brush = self.brush.as_ref().unwrap();
+
+            target.DrawRectangle(
+                &D2D_RECT_F {
+                    left: 100.0,
+                    top: 100.0,
+                    right: 200.0,
+                    bottom: 200.0,
+                },
+                brush,
+                1.0,
+                None,
+            );
+        }
+        Ok(())
+    }
+
+    fn present(&self, sync: u32, flags: u32) -> Result<()> {
+        unsafe { self.swapchain.as_ref().unwrap().Present(sync, flags).ok() }
+    }
+}
+
+fn create_factory() -> Result<ID2D1Factory1> {
+    let mut options = D2D1_FACTORY_OPTIONS::default();
+
+    if cfg!(debug_assertions) {
+        options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
+    }
+
+    unsafe { D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, Some(&options)) }
+}
+
+fn create_device_with_type(drive_type: D3D_DRIVER_TYPE) -> Result<ID3D11Device> {
+    let mut flags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+
+    if cfg!(debug_assertions) {
+        flags |= D3D11_CREATE_DEVICE_DEBUG;
+    }
+
+    let mut device = None;
+
+    unsafe {
+        D3D11CreateDevice(
+            None,
+            drive_type,
+            None,
+            flags,
+            None,
+            D3D11_SDK_VERSION,
+            Some(&mut device),
+            None,
+            None,
+        )
+            .map(|()| device.unwrap())
+    }
+}
+
+fn create_device() -> Result<ID3D11Device> {
+    let mut result = create_device_with_type(D3D_DRIVER_TYPE_HARDWARE);
+
+    if let Err(err) = &result {
+        if err.code() == DXGI_ERROR_UNSUPPORTED {
+            result = create_device_with_type(D3D_DRIVER_TYPE_WARP);
+        }
+    }
+
+    result
+}
+
+fn create_render_target(
+    factory: &ID2D1Factory1,
+    device: &ID3D11Device,
+) -> Result<ID2D1DeviceContext> {
+    unsafe {
+        let d2device = factory.CreateDevice(&device.cast::<IDXGIDevice>()?)?;
+        let target = d2device.CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE)?;
+        target.SetUnitMode(D2D1_UNIT_MODE_DIPS);
+
+        Ok(target)
+    }
+}
+
+fn create_brush(target: &ID2D1DeviceContext) -> Result<ID2D1SolidColorBrush> {
+    let color = D2D1_COLOR_F {
+        r: 0.92,
+        g: 0.38,
+        b: 0.208,
+        a: 1.0,
+    };
+
+    let properties = D2D1_BRUSH_PROPERTIES {
+        opacity: 1.0,
+        transform: Matrix3x2::identity(),
+    };
+
+    unsafe { target.CreateSolidColorBrush(&color, Some(&properties)) }
+}
+
+fn create_style(factory: &ID2D1Factory1) -> Result<ID2D1StrokeStyle> {
+    let props = D2D1_STROKE_STYLE_PROPERTIES {
+        startCap: D2D1_CAP_STYLE_ROUND,
+        endCap: D2D1_CAP_STYLE_TRIANGLE,
+        ..Default::default()
+    };
+
+    unsafe { factory.CreateStrokeStyle(&props, None) }
+}
+
+fn create_swapchain(device: &ID3D11Device, window: HWND) -> Result<IDXGISwapChain1> {
+    let factory = get_dxgi_factory(device)?;
+
+    let props = DXGI_SWAP_CHAIN_DESC1 {
+        Format: DXGI_FORMAT_B8G8R8A8_UNORM,
+        SampleDesc: DXGI_SAMPLE_DESC {
+            Count: 1,
+            Quality: 0,
+        },
+        BufferUsage: DXGI_USAGE_RENDER_TARGET_OUTPUT,
+        BufferCount: 2,
+        SwapEffect: DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL,
+        ..Default::default()
+    };
+
+    unsafe { factory.CreateSwapChainForHwnd(device, window, &props, None, None) }
+}
+
+fn create_swapchain_bitmap(swapchain: &IDXGISwapChain1, target: &ID2D1DeviceContext) -> Result<()> {
+    let surface: IDXGISurface = unsafe { swapchain.GetBuffer(0)? };
+
+    let props = D2D1_BITMAP_PROPERTIES1 {
+        pixelFormat: D2D1_PIXEL_FORMAT {
+            format: DXGI_FORMAT_B8G8R8A8_UNORM,
+            alphaMode: D2D1_ALPHA_MODE_IGNORE,
+        },
+        dpiX: 96.0,
+        dpiY: 96.0,
+        bitmapOptions: D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
+        ..Default::default()
+    };
+
+    unsafe {
+        let bitmap = target.CreateBitmapFromDxgiSurface(&surface, Some(&props))?;
+        target.SetTarget(&bitmap);
+    };
+
+    Ok(())
+}
+
+fn get_dxgi_factory(device: &ID3D11Device) -> Result<IDXGIFactory2> {
+    let dxdevice = device.cast::<IDXGIDevice>()?;
+    unsafe { dxdevice.GetAdapter()?.GetParent() }
 }
